b5b9a2eac31b7527c7a5dafb1aa062726451e2af
Louis <vanaaardtlj03@gmail.com>
2025-04-10 13:03:04 +0200

    Added getbook, getbookbyid, createbook controllers, and their respective functions in models package

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,45 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/F0RG-2142/book-management-system/pkg/models"
+	"github.com/F0RG-2142/book-management-system/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglocation/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing book name")
+	}
+	bookDetails, _ := models.GetBookById(id)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglocation/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	createBook := &models.Book{}
+	utils.ParseBody(r, createBook)
+	b := createBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -25,3 +25,21 @@ func (b *Book) CreateBook() *Book {
 	db.Create(&b)
 	return b
 }
+
+func GetAllBooks() []Book {
+	var books []Book
+	db.Find(&books)
+	return books
+}
+
+func GetBookById(id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(id int64) Book {
+	var book Book
+	db.Where("ID=?", id).Delete(book)
+	return book
+}
